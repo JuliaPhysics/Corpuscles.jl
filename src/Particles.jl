7369c32269736d798f58ee0885baf70db5e77a14
Johannes Schumann <johannes.schumann@fau.de>
2019-11-20 18:21:55 +0100

    Replaced eachrow for Julia v1.0 compatibiliy

--- src/Particles.jl
+++ src/Particles.jl
@@ -2,6 +2,7 @@ module Particles
 
 using DelimitedFiles
 
+rows(x) = (x[i, :] for i in 1:size(x)[1])
 
 function Base.parse(::Type{Rational{T}}, val::AbstractString) where {T <: Integer}
     !('/' in val) && return parse(T, val) // 1
@@ -63,7 +64,7 @@ function read_particle_csv(filepath::AbstractString)
     file_content = readdlm(filepath, ',', AbstractString)
     header = string.(file_content[1,:])
     dct_particles = ParticleDict()
-    for row in eachrow(file_content[2:end,:])
+    for row in rows(file_content[2:end,:])
         pdgid       = parse(Int64, row[1])
         mass_value  = parse(Float64, row[2])
         mass_lower  = parse(Float64, row[3])
