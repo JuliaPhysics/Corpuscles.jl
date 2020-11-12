0037fbdcbd1bdab28356d5410720479041c678d0
Johannes Schumann <johannes.schumann@fau.de>
2020-11-12 16:16:15 +0100

    Add other has quark helpers

--- src/helpers.jl
+++ src/helpers.jl
@@ -3,10 +3,18 @@
 
 isquark(p::Particle) = 1 <= abs(p.pdgid.value) <= 8
 
-function hastop(p::Particle)
-    retval = abs(p.pdgid.value) == 6
-    retval |= abs(p.pdgid.Nq1) == 6
-    retval |= abs(p.pdgid.Nq2) == 6
-    retval |= abs(p.pdgid.Nq3) == 6
+function _hasquark(p::Particle, id::Integer)
+    retval = abs(p.pdgid.value) == id
+    retval |= p.pdgid.Nq1 == id 
+    retval |= p.pdgid.Nq2 == id 
+    retval |= p.pdgid.Nq3 == id
     retval
 end 
+
+hasdowm(p::Particle) = _hasquark(p, 1)
+hasup(p::Particle) = _hasquark(p, 2)
+hascharm(p::Particle) = _hasquark(p, 3)
+hasstrange(p::Particle) = _hasquark(p, 4)
+hasbottom(p::Particle) = _hasquark(p, 5)
+hastop(p::Particle) = _hasquark(p, 6)
+
