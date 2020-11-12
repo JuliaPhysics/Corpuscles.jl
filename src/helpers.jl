10a03f0464ad506a52ccc072b4bbe99dd08869d9
Johannes Schumann <johannes.schumann@fau.de>
2020-11-12 15:59:50 +0100

    hastop function

--- src/helpers.jl
+++ src/helpers.jl
@@ -2,3 +2,11 @@
 
 
 isquark(p::Particle) = 1 <= abs(p.pdgid.value) <= 8
+
+function hastop(p::Particle)
+    retval = abs(p.pdgid.value) == 6
+    retval |= abs(p.pdgid.Nq1) == 6
+    retval |= abs(p.pdgid.Nq2) == 6
+    retval |= abs(p.pdgid.Nq3) == 6
+    retval
+end 
