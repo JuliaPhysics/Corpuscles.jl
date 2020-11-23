c731fcfe332b3c3aa71469264dea4533c8d54826
Tamas Gal <tgal@km3net.de>
2020-11-23 12:31:20 +0100

    Fix Julia 1.0 and 1.1 support

--- src/Corpuscles.jl
+++ src/Corpuscles.jl
@@ -52,7 +52,9 @@ struct PDGID <: ParticleID
     N10::Int8
 
     function PDGID(value)
-        return new(value, digits(abs(value), pad=10)...)
+        d = digits(abs(value), pad=10)
+        # splatting in `new()` is only supported in Julia 1.2+
+        return new(value, d[1], d[2], d[3], d[4], d[5], d[6], d[7], d[8], d[9], d[10])
     end
 end
 
