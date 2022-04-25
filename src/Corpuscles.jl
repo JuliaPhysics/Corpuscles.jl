f47cf1e81f7e4275885fa55d7d58350c646df333
Tamas Gal <himself@tamasgal.com>
2022-04-25 17:07:17 +0200

    Prepare the fancier name with charge and anti-flag

--- src/Corpuscles.jl
+++ src/Corpuscles.jl
@@ -233,7 +233,7 @@ function read_particle_csv(filepath::AbstractString)
         charge = parse(Int8, row[13]) // 3 * u"e_au"
         rank = parse(Int8, row[14])
         status = PDGStatus(parse(Int8, row[15]))
-        name = row[16]
+        name = _generatename(row[16], pdgid.value, antiprop, charge)
         glyph = get(PIDNames, pdgid.value, row[16])
         quarks = row[17]
         latex = row[18]
@@ -255,6 +255,20 @@ function read_particle_csv(filepath::AbstractString)
     end
     dct_particles
 end
+
+"""
+$(SIGNATURES)
+
+Generate a name for the particle, adding tilde if anti and -/+ according to the
+charge.
+"""
+function _generatename(name, pdgid, antiprop, charge)
+    suffix_tilde = antiprop == Barred && pdgid < 0 ? "~" : ""
+    suffix_charge = ""
+    return name * suffix_tilde * suffix_charge
+end
+
+
 
 """
 $(SIGNATURES)
